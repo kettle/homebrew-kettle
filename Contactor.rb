ea73cff418d52553961e7f53a6ac3dd939ed4d2b
Tyler Peterson <peterson_tyler@apple.com>
2018-04-18 03:23:40 -0400

    Release 1.0.3

--- Contactor.rb
+++ Contactor.rb
@@ -2,7 +2,7 @@ class Contactor < Formula
 	desc "Manage contacts from the command line via the macOS Contacts framework."
 	homepage "https://github.com/kettle/Contactor"
 	url "https://github.com/kettle/Contactor/raw/master/archive/Contactor-1.0.3.tar.gz"
-	sha256 "b95cc82244ca936039dc9aafd92d54b7caff5f84c503b9ed043a4c8dbbd8d915"
+	sha256 "8e9df73aad0ecb3ca10325b491023d52f22c3a62ea7058b140d18771873cc131"
 	version "1.0.3"
 
 	depends_on "curl"
